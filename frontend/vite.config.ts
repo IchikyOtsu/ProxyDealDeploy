56c67ae521940b57fe038f339a6e506f3b39dd79
Ichiky_Otsu <66952647+IchikyOtsu@users.noreply.github.com>
2024-04-09 14:44:51 +0200

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [solidPlugin()],
   server: {
     proxy: {
-      '/api': 'http://172.24.0.2:3000'
+      '/api': 'http://172.16.238.50:3000'
     }
   }
 });
